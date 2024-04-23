bea4d9c37431ce7e7a8cb4f440e4304789f40654
David Lim <david.lim@berkeley.edu>
2024-04-22 19:46:46 -0700

    first stepping stone

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/SearchBar.css";
+
+function SearchBar() {
+  return (
+    <>
+      <input type="text" name="ingredients"></input>
+      hello
+    </>
+  );
+}
+
+export default SearchBar;
