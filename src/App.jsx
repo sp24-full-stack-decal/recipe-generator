41a71b9b9f2041da13928966b0f8cf37b876c586
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-24 11:33:36 -0700

    Added div

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./index.css";
 import SearchBar from "./components/SearchBar";
+import FullRecipe from "./components/FullRecipe";
 
 function App() {
   return (
-    <>
       <div>
         <SearchBar />
+        {/* <FullRecipe /> */}
       </div>
-    </>
   );
 }
 
---
+++ src/components/FullRecipe.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./css/FullRecipe.css";
+
+function FullRecipe() {
+  return (
+    <div className="full-recipe">
+      <div className="upper searchbar">
+        <SearchBar/>
+      </div>
+      <div clasName="main body">
+        <div className="main left">
+        </div>
+        <div className="main right">
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default FullRecipe
