7e687f6583660e6aaece4e5b6f274f6e8714bebc
David Lim <david.lim@berkeley.edu>
2024-04-26 11:52:15 -0700

    done with styling for search box

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,39 @@
 import React from "react";
-import "./css/SearchBar.css";
+import "./css/SearchBox.css";
 
 function SearchBar() {
   return (
     <>
-      <input type="text" name="ingredients"></input>
-      hello
+      <form id="search-box">
+        <input
+          id="search-bar"
+          type="text"
+          name="ingredients"
+          placeholder="What's in your fridge?"
+        />
+        <input id="generate" type="submit" value="Generate" />
+      </form>
+      <div id="ingredient-filter">
+        <input type="checkbox" id="ingredient-check" value="ingredient-check" />
+        <label for="ingredient-check">
+          Include ingredients that I don't have
+        </label>
+      </div>
     </>
   );
 }
+
+// NEEDS WORK
+// const response = await fetch("https://api.edamam.com/api/recipes/v2s", {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Authorization: "Bearer rg_v1_zhj0n9fa7frxk2swzmelhm6bdzwtpcry20hl_ngk",
+//   },
+//   body: JSON.stringify({
+//     // fill variables here.
+//     email: emailBody,
+//   }),
+// }).then((response) => response.text());
 
 export default SearchBar;
