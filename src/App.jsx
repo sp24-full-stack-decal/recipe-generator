f6705d86e2fc841f0c13ef59d85dc8e05478bc20
David Lim <david.lim@berkeley.edu>
2024-04-26 23:24:36 -0700

    actually deleted the file now

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./index.css";
 import SearchBar from "./components/SearchBar";
-import RecipeSearch from "./RecipeSearch";
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import RecipeDetail from "./pages/RecipeDetail.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/recipes/:id",
-    element: <RecipeDetail />,
   },
 ]);
 
