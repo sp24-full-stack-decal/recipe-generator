28a9bf2fb0726bfa6e7993ce5a16927a5b84e01f
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-29 05:22:14 -0700

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import "./index.css";
-import SearchBar from "./components/SearchBar";
-import FullRecipe from "./components/FullRecipe";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   return (
       <div className="app">
-        <div id="landing-box">
-          <h1>
-            Generate your<span id="personalized"> personalized </span>recipe
-          </h1>
-          <SearchBar />
-        </div>
+        <SearchPage />
       </div>
   );
 }
